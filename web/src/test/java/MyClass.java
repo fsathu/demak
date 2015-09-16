8b41a957620b1c3d63e82e5c17efc30965a45f48
Fredrick <fredrick@hsenid.lk>
2015-09-16 15:48:30 +0530

    re

--- web/src/test/java/MyClass.java
+++ web/src/test/java/MyClass.java
@@ -1,40 +1,45 @@
-/**
- * Created by fredrick on 9/15/15.
- */
-class MyClass {
-    private int myPrivateVar;
-    public int myPublicVar;
-    protected int myProtectedVar;
-
-    private void myPrivateMethod(){
-        myPrivateVar++;
-
-    }
-    public void myPublicVar(){
-
-    }
-    protected void myProtectedVar(){
-
+public interface ISoundBehaviour
+{
+    public void makeSound();
+}
+public class MeowSound implements ISoundBehaviour
+{
+    public void makeSound()
+    {
+        System.out.println("Meow");
     }
 }
-class MySubClass extends MyClass{
-
-   private void MySubClassMethod(){
-       myProtectedVar++;
-       myPublicVar++;
-   }
-
-
-}
-
-class AnotherClass{
-    public void anotherClassMethod(){
-        MyClass myClass=new MyClass();
-        myClass.myProtectedVar();
-        myClass.myPublicVar++;
+public class RoarSound implements ISoundBehaviour
+{
+    public void makeSound()
+    {
+        System.out.println("Roar!");
+    }}
+public class Cat
+{
+    private ISoundBehaviour sound = new MeowSound();
+    public void makeSound()
+    {
+        sound.makeSound();
     }
-
-
+    public void setSoundBehaviour(ISoundBehaviour newsound)
+    {
+        sound = newsound;
+    }
 }
-
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Cat c = new Cat();
+// Delegation
+        c.makeSound();
+// Output: Meow
+// change the sound it makes
+        ISoundBehaviour newsound = new RoarSound();
+        c.setSoundBehaviour(newsound);
+// Delegation
+        c.makeSound();
+// Output: Roar!
+    }
+}
